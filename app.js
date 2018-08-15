597ce533c969d6676e676b2d72c38af010ede19c
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-08-15 19:38:34 -0400

    first commit

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const session    = require('express-session');
+const passport   = require('passport');
 
 
 mongoose.Promise = Promise;
@@ -18,6 +20,19 @@ mongoose
   }).catch(err => {
     console.error('Error connecting to mongo', err)
   });
+
+const passportSetup = require('./config/passport');
+passportSetup(passport);
+
+app.use(session({
+  secret: 'angular auth passport secret shh',
+  resave: true,
+  saveUninitialized: true,
+  cookie : { httpOnly: true, maxAge: 2419200000 }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -31,28 +46,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-
 app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
-
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-
-
+//  Routes
+const authRoutes = require('./routes/auth-routes');
 const index = require('./routes/index');
+app.use('/', authRoutes);
 app.use('/', index);
 
+app.use((req, res, next) => {
+  res.sendfile(__dirname + '/public/index.html');
+});
 
 module.exports = app;
